a3b3bbc2cef65f1f017a55dd9afc98c52d318da6
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-08-14 18:16:12 +0200

    Fixed the handleOperator (signs combinations)

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,11 @@ function App() {
 
   useEffect(() => {
     console.log(`${display} ${typeof display}`);
-    
-  }, [display])
-  
+  }, [display]);
 
   const handleNumber = (number: string) => {
     console.log(display);
-    if (display === '0') {
+    if (display === "0") {
       setDisplay(number);
     } else {
       setDisplay(display + number);
@@ -20,29 +18,48 @@ function App() {
   };
 
   const handleOperator = (operator: string) => {
-    setDisplay(`${display} ${operator} `);
-  }
+    // if(/[+*/-]/.test(display[display.length - 4]) && display[display.length - 2] === '-'){
+    //   setDisplay(`${display.slice(0, -5)} ${operator} `);
+    // } 
+    if (/[+*/-]/.test(display[display.length - 2]) && operator !== "-") {
+      setDisplay(`${display.slice(0, -3)} ${operator} `);
+    }  else if (operator === "-" && /[+\*/]/.test(display[display.length - 2])) {
+      setDisplay(`${display} -`);
+    } else if (operator === "-" && display[display.length - 2] !== "-") {
+      setDisplay(`${display.slice(0, -3)} ${operator} `);
+    } else {
+      setDisplay(`${display} ${operator} `);
+    }
+  };
 
   const handleEqual = () => {
-    setDisplay(eval(display).toString());
-  }
+    setDisplay(eval(display).toString().trim());
+  };
 
   const handleDecimal = () => {
-    const array = display.toString().split(' ');
+    const array = display.toString().split(" ");
     const lastElement = array[array.length - 1];
 
-    if(!lastElement.includes(".")) {
-      setDisplay(display + '.');
+    if (!lastElement.includes(".")) {
+      setDisplay(display + ".");
     }
-  }
+  };
 
   const handleCleanAll = () => {
     setDisplay("0");
-  }
+  };
 
   const handleClean = () => {
-    display === '' ? '0' : setDisplay(prev => prev.toString().split("").slice(0, prev.toString().length - 1).join(""));
-  }
+    display === ""
+      ? "0"
+      : setDisplay((prev) =>
+          prev
+            .toString()
+            .split("")
+            .slice(0, prev.toString().length - 1)
+            .join("")
+        );
+  };
 
   return (
     <div className="container">
@@ -54,37 +71,77 @@ function App() {
           <div className="grid__btn clean" id="clear" onClick={handleCleanAll}>
             AC
           </div>
-          <div className="grid__btn clean" id="clean1Digit" onClick={handleClean}>
+          <div
+            className="grid__btn clean"
+            id="clean1Digit"
+            onClick={handleClean}
+          >
             C
           </div>
-          <div className="grid__btn dark-gray" id="divide" onClick={() => handleOperator("/")}>
+          <div
+            className="grid__btn dark-gray"
+            id="divide"
+            onClick={() => handleOperator("/")}
+          >
             /
           </div>
-          <div className="grid__btn dark-gray" id="multiply" onClick={() => handleOperator("*")}>
+          <div
+            className="grid__btn dark-gray"
+            id="multiply"
+            onClick={() => handleOperator("*")}
+          >
             *
           </div>
-          <div className="grid__btn" id="seven" onClick={() => handleNumber("7")}>
+          <div
+            className="grid__btn"
+            id="seven"
+            onClick={() => handleNumber("7")}
+          >
             7
           </div>
-          <div className="grid__btn" id="eight" onClick={() => handleNumber("8")}>
+          <div
+            className="grid__btn"
+            id="eight"
+            onClick={() => handleNumber("8")}
+          >
             8
           </div>
-          <div className="grid__btn" id="nine" onClick={() => handleNumber("9")}>
+          <div
+            className="grid__btn"
+            id="nine"
+            onClick={() => handleNumber("9")}
+          >
             9
           </div>
-          <div className="grid__btn dark-gray" id="subtract" onClick={() => handleOperator("-")}>
+          <div
+            className="grid__btn dark-gray"
+            id="subtract"
+            onClick={() => handleOperator("-")}
+          >
             -
           </div>
-          <div className="grid__btn" id="four" onClick={() => handleNumber("4")}>
+          <div
+            className="grid__btn"
+            id="four"
+            onClick={() => handleNumber("4")}
+          >
             4
           </div>
-          <div className="grid__btn" id="five" onClick={() => handleNumber("5")}>
+          <div
+            className="grid__btn"
+            id="five"
+            onClick={() => handleNumber("5")}
+          >
             5
           </div>
           <div className="grid__btn" id="six" onClick={() => handleNumber("6")}>
             6
           </div>
-          <div className="grid__btn dark-gray" id="add" onClick={() => handleOperator("+")}>
+          <div
+            className="grid__btn dark-gray"
+            id="add"
+            onClick={() => handleOperator("+")}
+          >
             +
           </div>
           <div className="grid__btn" id="one" onClick={() => handleNumber("1")}>
@@ -93,13 +150,21 @@ function App() {
           <div className="grid__btn" id="two" onClick={() => handleNumber("2")}>
             2
           </div>
-          <div className="grid__btn" id="three" onClick={() => handleNumber("3")}>
+          <div
+            className="grid__btn"
+            id="three"
+            onClick={() => handleNumber("3")}
+          >
             3
           </div>
           <div className="grid__btn" id="equals" onClick={handleEqual}>
             =
           </div>
-          <div className="grid__btn" id="zero" onClick={() => handleNumber("0")}>
+          <div
+            className="grid__btn"
+            id="zero"
+            onClick={() => handleNumber("0")}
+          >
             0
           </div>
           <div className="grid__btn" id="decimal" onClick={handleDecimal}>
